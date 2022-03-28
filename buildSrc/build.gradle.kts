9bb6ffe88b43680edc665d29f501c77cdf74db14
Muhammad Faisal Amir <faisalamircs@gmail.com>
2022-03-28 15:44:41 +0700

    update: build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,9 +1,19 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
 
 repositories {
     mavenCentral()
 }
 
-plugins {
-    `kotlin-dsl`
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
 }
