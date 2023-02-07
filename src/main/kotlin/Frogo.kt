bdf217c8ef6a66bb4131c2e7a01e16f098a6036f
amirisback <faisalamircs@gmail.com>
2023-02-07 14:06:55 +0700

    Release v1.3.9

--- src/main/kotlin/Frogo.kt
+++ src/main/kotlin/Frogo.kt
@@ -14,9 +14,9 @@
 object Frogo {
 
     const val recyclerViewVersion = "4.3.3" // https://github.com/amirisback/frogo-recycler-view
-    const val admobVersion = "5.1.6" // https://github.com/amirisback/frogo-admob
+    const val admobVersion = "5.1.7" // https://github.com/amirisback/frogo-admob
     const val uiVersion = "1.1.2" // https://github.com/frogobox/frogo-ui
-    const val sdkVersion = "2.1.2" // https://github.com/frogobox/frogo-sdk
+    const val sdkVersion = "2.1.4" // https://github.com/frogobox/frogo-sdk
     const val consumeApiVersion = "2.4.3" // https://github.com/frogobox/frogo-consume-api
 
     const val logVersion = "2.0.9" // https://github.com/amirisback/frogo-log [DEPRECATED]
