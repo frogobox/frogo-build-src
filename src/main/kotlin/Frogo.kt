e883f56f04cec2cda26a09d257d5239fa6ebf2d0
Muhammad Faisal Amir <faisalamircs@gmail.com>
2023-04-19 18:56:02 +0700

    Release v1.4.3
    Co-authored-by: frogoboxbot <frogoboxmedia@gmail.com>

--- src/main/kotlin/Frogo.kt
+++ src/main/kotlin/Frogo.kt
@@ -13,11 +13,11 @@
 
 object Frogo {
 
-    const val recyclerViewVersion = "4.3.4" // https://github.com/amirisback/frogo-recycler-view
-    const val admobVersion = "5.2.0" // https://github.com/amirisback/frogo-admob
-    const val uiVersion = "1.1.4" // https://github.com/frogobox/frogo-ui
-    const val sdkVersion = "2.1.6" // https://github.com/frogobox/frogo-sdk
-    const val consumeApiVersion = "2.4.5" // https://github.com/frogobox/frogo-consume-api
+    const val recyclerViewVersion = "4.3.5" // https://github.com/amirisback/frogo-recycler-view
+    const val admobVersion = "5.2.1" // https://github.com/amirisback/frogo-admob
+    const val uiVersion = "1.1.5" // https://github.com/frogobox/frogo-ui
+    const val sdkVersion = "2.1.7" // https://github.com/frogobox/frogo-sdk
+    const val consumeApiVersion = "2.4.6" // https://github.com/frogobox/frogo-consume-api
 
     const val logVersion = "2.0.9" // https://github.com/amirisback/frogo-log [DEPRECATED]
     const val notificationVersion = "1.1.1" // https://github.com/amirisback/frogo-notification [DEPRECATED]
